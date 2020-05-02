10fe5706f77c8ca5ad94dfe570fb05bc44e993d8
mohamed31195 <mohamed31195@hotmail.com>
2020-05-02 18:52:05 +0800

    Add routing for patient views

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
+  root to: "patients#index"
+
+  get 'patients/index'
+
+  get 'patients/file_import'
+
+  get 'patients/show'
+    
+  get 'patients/import' => 'patients#my_import'
+
+    resources :patients do
+      collection {post :import}
+    end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
